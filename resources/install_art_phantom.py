f4fa7c98766757fc5fefc060182dff130bd8b1b7
Tim Frazier <tim.frazier@phantom.us>
2019-09-28 20:55:17 -0400

    fixing error in python scripts

--- resources/install_art_phantom.py
+++ resources/install_art_phantom.py
@@ -2,7 +2,7 @@
 import json
 import base64
 import requests
-file_contents = open('/phantom_apps/phatomicredteam.tgz', 'rb').read()
+file_contents = open('./phantom_apps/phatomicredteam.tgz', 'rb').read()
 encoded_contents = base64.b64encode(file_contents)
 payload = {'app': encoded_contents}
 requests.post('https://192.168.38.110/rest/app',
