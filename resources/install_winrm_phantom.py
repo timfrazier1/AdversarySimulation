33de22c60713dff85a98d0482469fd01bb9c0e1f
Tim Frazier <tim.frazier@phantom.us>
2019-09-28 20:49:25 -0400

    Updating for installing Phantom apps

---
+++ resources/install_winrm_phantom.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import json
+import base64
+import requests
+file_contents = open('/phantom_apps/phwinrm.tgz', 'rb').read()
+encoded_contents = base64.b64encode(file_contents)
+payload = {'app': encoded_contents}
+requests.post('https://192.168.38.110/rest/app',
+                auth=('admin', 'password'),
+                data=json.dumps(payload),
+                verify=False)
