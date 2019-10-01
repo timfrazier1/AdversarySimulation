f6887aeddd99cba9e045acdfa88c7c4a47ecdc89
Tim Frazier <tim.frazier@phantom.us>
2019-09-30 21:22:46 -0400

    Updating app install script

--- resources/install_phantom_app.py
+++ resources/install_phantom_app.py
@@ -6,7 +6,9 @@ import requests
 file_contents = open(sys.argv[1], 'rb').read()
 encoded_contents = base64.b64encode(file_contents)
 payload = {'app': encoded_contents}
-requests.post('https://192.168.38.110/rest/app',
+r = requests.post('https://192.168.38.110/rest/app',
                 auth=('admin', sys.argv[2]),
                 data=json.dumps(payload),
                 verify=False)
+print r.status_code
+print r.text
